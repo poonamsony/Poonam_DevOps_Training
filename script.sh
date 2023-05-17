9c848669a80f82ddc16a125456329ab2f5f276b2
poonamsony <124334796+poonamsony@users.noreply.github.com>
2023-05-17 15:15:48 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo [INFO] Running this using a Script at `date`
+echo [INFO] Current location :: `pwd`
+echo [INFO] Files at current path:
+ls -ltra
+echo [INFO] Current user :: `whoami`
