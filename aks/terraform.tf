a2d6c3909e3cb6d3623cfbb8bdd3ba59689bbbb9
poonamsony <124334796+poonamsony@users.noreply.github.com>
2023-05-18 10:56:14 +0530

    Update terraform.tf

--- aks/terraform.tf
+++ aks/terraform.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name = "thinknyxlabs"
     storage_account_name = "thinknyxlabs"
     container_name = "thinknyxlabs"
-    key = "jishnu.terraform.tfstate"
+    key = "poonam.terraform.tfstate"
   }
 }
