800b5310b2bf474142a9d9cf6990395e14891e10
poonamsony <124334796+poonamsony@users.noreply.github.com>
2023-05-18 09:46:36 +0530

    Uploaded the Aks file

---
+++ aks/output.tf
@@ -0,0 +1,3 @@
+output "kubernetes_namespaces" {
+  value = data.kubernetes_all_namespaces.namespaces.namespaces
+}
---
+++ aks/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "Enter your name to be used with name arguments & tags"
+}
+variable "client_id" {
+  description = "Enter Service Principal Client ID"
+}
+variable "client_secret" {
+  description = "Enter the Service Principal Client Secret"
+}
+locals {
+  tags = {
+    client = "sony"
+    user   = var.name
+  }
+  name = "thinknyx-${var.name}"
+}
